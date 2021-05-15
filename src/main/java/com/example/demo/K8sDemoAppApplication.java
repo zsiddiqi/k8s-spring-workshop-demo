64ab055dce1049094c5ee86bee82af388ef025fa
Zafar Siddiqi <zafar.siddiqi@gmail.com>
2021-05-15 05:09:43 +0000

    completed #42

--- src/main/java/com/example/demo/K8sDemoAppApplication.java
+++ src/main/java/com/example/demo/K8sDemoAppApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 @RestController
 @SpringBootApplication
 public class K8sDemoAppApplication {
+private RestTemplate rest = new RestTemplateBuilder().build();
 
 	public static void main(String[] args) {
 		SpringApplication.run(K8sDemoAppApplication.class, args);
@@ -16,6 +19,7 @@ public class K8sDemoAppApplication {
 
 @GetMapping("/")
 public String hello() {
-    return "Hola World\n";
+String name = rest.getForObject("http://k8s-workshop-name-service", String.class);
+return "Hola " + name;
 }
 }
